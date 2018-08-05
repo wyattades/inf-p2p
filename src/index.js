945e0ba30ac2f9394190d39d7171fc573ebfe351
Wyatt Ades <wyattades@gmail.com>
2018-08-05 00:47:54 -0700

    update global var

--- src/index.js
+++ src/index.js
@@ -9,4 +9,4 @@ game.init();
 
 // stats
 // eslint-disable-next-line
-DEV && (function(){var script=document.createElement('script');script.onload=function(){var stats=new Stats();document.body.appendChild(stats.dom);requestAnimationFrame(function loop(){stats.update();requestAnimationFrame(loop)});};script.src='//rawgit.com/mrdoob/stats.js/master/build/stats.min.js';document.head.appendChild(script);})();
+process.env.DEV && (function(){var script=document.createElement('script');script.onload=function(){var stats=new Stats();document.body.appendChild(stats.dom);requestAnimationFrame(function loop(){stats.update();requestAnimationFrame(loop)});};script.src='//rawgit.com/mrdoob/stats.js/master/build/stats.min.js';document.head.appendChild(script);})();
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ module.exports = require('webpack-boiler')({
   pages: [{
     title: 'P2P Test',
     description: '<Temporary description>',
-    template: __dirname + '/src/index.pug',
+    template: `${__dirname}/src/index.pug`,
   }],
+  basename: 'inf-p2p',
 });
