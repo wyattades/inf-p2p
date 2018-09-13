5cf4738dad08a00ce74f9bfddb691a707a6c39f7
Wyatt Ades <wyattades@gmail.com>
2018-09-13 12:57:39 -0400

    Better options menu

--- src/ui.js
+++ src/ui.js
@@ -1,7 +1,27 @@
+import * as options from './options';
 
-const $info = document.createElement('div');
-$info.id = 'info';
-document.body.appendChild($info);
+
+const $info = document.getElementById('info');
+const $menu = document.getElementById('menu');
+
+document.querySelectorAll('#options [name]').forEach((el) => {
+  const type = el.getAttribute('type') || 'select';
+  const valProp = type === 'checkbox' ? 'checked' : 'value';
+  const key = el.getAttribute('name');
+  el[valProp] = options.get(key);
+  if (type === 'range') {
+    el.nextSibling.textContent = el[valProp];
+    el.oninput = () => {
+      el.nextSibling.textContent = el[valProp];
+    };
+  }
+  el.onchange = () => {
+    let val = el[valProp];
+    if (type === 'range') val = Number.parseInt(val, 10);
+    options.set(key, val);
+  };
+});
+$menu.querySelector('button').onclick = () => window.cheat.resume();
 
 const vals = {
   chunkX: null,
@@ -30,4 +50,16 @@ export const key = (keyCode, action) => {
   document.addEventListener('keydown', (e) => {
     if (e.which === keyCode.charCodeAt(0)) action();
   });
+};
+
+// export const toggleMenu = () => {
+//   $menu.classList.toggle('hidden');
+// };
+
+export const setActiveMenu = (active) => {
+  $menu.classList[active ? 'remove' : 'add']('hidden');
+};
+
+export const toggleInfo = () => {
+  $info.classList.toggle('hidden');
 };
