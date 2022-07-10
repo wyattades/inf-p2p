5ea3201a9b8c312026c4ca16803883812ae00119
Wyatt Ades <wyattades@gmail.com>
2022-07-10 12:41:05 -0700

    Updates

--- _next/static/tL2Ja9_d4KrB_MrxF_ZSC/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/29107295-fbcfe2172188e46f.js","static/chunks/pages/index-f1bbc90410e52fe6.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
